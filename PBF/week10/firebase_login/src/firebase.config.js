bc3f63f69c1863c54ea7ea81b0cce4715507784e
ArionoS <arionoseptian0802@gmail.com>
2022-05-30 16:46:14 +0700

    job 10

---
+++ PBF/week10/firebase_login/src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: "AIzaSyCMpgUWxooWFzLTgLUwzEReS8vTnO3bpHk",
+  authDomain: "fir-login-ae901.firebaseapp.com",
+  projectId: "fir-login-ae901",
+  storageBucket: "fir-login-ae901.appspot.com",
+  messagingSenderId: "585745051934",
+  appId: "1:585745051934:web:10aaca4f94b95a17db190f",
+  measurementId: "G-KB4VBPQ22Y"
+};
